7fa07c0a5cd77319f0dd50ab9e8f48b6db155e48
ADgainaihongshixian <adgainaihongshixian@gmail.com>
2024-01-17 11:52:08 +0800

    feat: 故障风格图片+ 融合风格图片 done

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
+import { Tooltip } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 import UpDownItem from '@/components/common/upDownItem';
+import ShowCom from '@/components/common/showCom';
 import intl from 'react-intl-universal';
 import './index.scss';
 
@@ -8,11 +11,46 @@ type UxListType = {
 };
 
 const UxList: FC<UxListType> = (props) => {
-  const { aa } = props;
   const cs = 'ux-list-container';
+  const renderMap = [
+    {
+      title: intl.get('Fault_Style_Picture'),
+      tipTitle: intl.get('Mixed_Mode'),
+      content: <div className='fault-picture'></div>,
+    },
+    {
+      title: intl.get('Fusion_Style_Picture'),
+      tipTitle: `${intl.get('Mixed_Mode')} + ${intl.get('Contrast')}`,
+      content: (
+        <div className='fusion-picture'>
+          <div className='main-picture'></div>
+        </div>
+      ),
+    },
+    { title: intl.get('Fault_Ripple'), tipTitle: intl.get('Filter', { p: 'SVG' }), content: '22' },
+  ];
+
   return (
     <div className={cs}>
-      <UpDownItem title={'11'} content={'22'} />
+      {renderMap?.map((render: any, index: number) => {
+        const { title, tipTitle = '', content } = render;
+        return (
+          <UpDownItem
+            key={`${title}-${index}`}
+            title={
+              <>
+                <span>{title} </span>
+                <ShowCom show={!!tipTitle}>
+                  <Tooltip title={tipTitle}>
+                    <InfoCircleOutlined />
+                  </Tooltip>
+                </ShowCom>
+              </>
+            }
+            content={content}
+          />
+        );
+      })}
     </div>
   );
 };
